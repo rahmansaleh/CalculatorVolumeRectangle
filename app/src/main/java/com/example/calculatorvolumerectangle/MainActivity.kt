42563be984a9741a8ad254ac1aa68361f5f5b448
muhammad rahman <rahman9600@gmail.com>
2019-07-21 22:04:49 +0700

    insert code

--- app/src/main/java/com/example/calculatorvolumerectangle/MainActivity.kt
+++ app/src/main/java/com/example/calculatorvolumerectangle/MainActivity.kt
@@ -2,11 +2,103 @@ package com.example.calculatorvolumerectangle
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.lang.NumberFormatException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private fun toDouble(str: String): Double? {
+        return try {
+            str.toDouble()
+        } catch (e: NumberFormatException) {
+            null
+        }
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_calculate) {
+            val inputLength = edtLength.text.toString().trim { it <= ' ' }
+            val inputWidth = edtWidth.text.toString().trim { it <= ' ' }
+            val inputHeight = edtHeight.text.toString().trim { it <= ' ' }
+
+            var isEmptyFields = false
+            var isInvalidDOuble = false
+
+            if (TextUtils.isEmpty(inputLength)) {
+                isEmptyFields = true
+                edtLength.error = "Field ini tidak boleh kosong"
+            }
+
+            if (TextUtils.isEmpty(inputWidth)) {
+                isEmptyFields = true
+                edtWidth.error = "Field ini tidak boleh kosong"
+            }
+
+            if (TextUtils.isEmpty(inputHeight)) {
+                isEmptyFields = true
+                edtHeight.error = "Field ini tidak boleh kosong"
+            }
+
+            var length = toDouble(inputLength)
+            var width = toDouble(inputWidth)
+            var height = toDouble(inputHeight)
+
+            if (length == null) {
+                isInvalidDOuble = true
+                edtLength.error = "Filed ini harus berupa nomor yang valid"
+            }
+
+            if (width == null) {
+                isInvalidDOuble = true
+                edtWidth.error = "Filed ini harus berupa nomor yang valid"
+            }
+
+            if (height == null) {
+                isInvalidDOuble = true
+                edtHeight.error = "Filed ini harus berupa nomor yang valid"
+            }
+
+            if (!isEmptyFields && !isInvalidDOuble) {
+                val volume = length as Double * width as Double * height as Double
+                tvResult.text = volume.toString()
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT, tvResult.text.toString())
+    }
+
+    private lateinit var edtWidth: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+
+    companion object {
+        private const val STATE_RESULT = "state_result"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edtWidth = findViewById(R.id.edt_width)
+        edtHeight = findViewById(R.id.edt_height)
+        edtLength = findViewById(R.id.edt_length)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
+
+        btnCalculate.setOnClickListener(this)
+
+        if (savedInstanceState != null) {
+            val result = savedInstanceState.getString(STATE_RESULT) as String
+            tvResult.text = result
+        }
     }
 }
